63693fe6d79cf309d2eaff68d30fed6bf7a02979
Hyejeong Park <phj1698@gmail.com>
2016-03-06 18:09:12 +0900

    use bootstrap

--- main/views.py
+++ main/views.py
@@ -5,5 +5,5 @@ import requests
 
 def index_page(request):
     response = requests.get('https://www.googleapis.com/youtube/v3/search?part=id%2Csnippet&maxResults=25&q=dogs&key=AIzaSyAZCjuZgWCzKWQxTm7jRXe3TDWO3Hlu15M')
-    context = {'items': response.json()['items']}
+    context = {'video_items': response.json()['items']}
     return render(request, 'main/index.html', context)
